0ec4deae63408a931d1c6c731bbc1a78ffd3fd65
Mykyta Zaitsev <nikitazaycev0503@gmail.com>
2026-07-22 23:16:34 +0300

    Small fixes

--- src/test/specs/test.spec.js
+++ src/test/specs/test.spec.js
@@ -1,22 +1,4 @@
-const LoginPage = require('../../PO/pages/login.page')
-
-const InventoryPage = require('../../PO/pages/inventory.page')
-
-const CartPage = require('../../PO/pages/cart.page')
-
-const FirstCheckout = require('../../PO/pages/first_checkout.page')
-
-const SecondCheckoutPage = require('../../PO/pages/second_checkout.page')
-
-const CompletedCheckoutPage = require('../../PO/pages/completed_checkout.page')
-
-const {pages } = require('../../PO/index')
-
-const inventoryPage = new InventoryPage()
-const cartPage = new CartPage()
-const firstCheckout = new FirstCheckout()
-const secondCheckout = new SecondCheckoutPage()
-const completedCheckout = new CompletedCheckoutPage()
+const { pages } = require('../../PO/index')
 
 describe("End-to-end checkout", () => {
   it("Should complete checkout successfully", async () => {
